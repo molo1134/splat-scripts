fce4c804ccd3b8c262c7766e7462b62810436c80
molo1134 <molo1134@gmx.com>
2016-01-11 18:05:12 -0500

    move hgt data gathering to parameterize by continent. run srtm2sdf for each hgt file as it is unpacked.

--- get-datafiles-northamerica.sh
+++ get-datafiles-northamerica.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
 # get the SRTM data files and convert them for splat use
-# requires ~11 GB free disk space
 # results in ~3.6 GB disk space consumed
 
-# path to topgraphy datafiles
-TOPOFILEDIR=splat-datafiles/sdf/
-# local hgt file archive
-HGTFILEDIR=splat-datafiles/hgtzip/
 # for state/county borders
 BORDERDIR=splat-datafiles/borders/
 
@@ -15,48 +10,11 @@ BORDERURL="https://web.archive.org/web/20130331144934/http://www.census.gov/geo/
 
 mkdir -p $BORDERDIR
 cd $BORDERDIR
+echo "retrieving state/county borders.."
 wget -N -nv $BORDERURL
+echo "unpacking state/county borders.."
 unzip -o *.zip
 cd -
 
-INDEXURL="http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/North_America/"
-
-INDEXFILE=`mktemp`
-
-echo "getting index.."
-wget -q -O - $INDEXURL | \
-	sed -e '/hgt.zip/!d; s/.* \([NSWE0-9]\+\.\?hgt\.zip\).*$/\1/;' \
-	> $INDEXFILE
-
-mkdir -p $HGTFILEDIR
-mkdir -p $TOPOFILEDIR
-
-cd $HGTFILEDIR
-wget -nv -N -B $INDEXURL -i $INDEXFILE
-cd -
-
-
-# TODO: to minimize disk space required, run srtm2sdf on each file as it is
-# unzipped.
-
-echo "unpacking hgt files.."
-cd $HGTFILEDIR
-for e in *.zip ; do 
-	echo $e
-	unzip -o $e
-done
-cd -
-
-echo "converting hgt files to sdf.."
-HGTREALPATH=`realpath $HGTFILEDIR`
-cd $TOPOFILEDIR
-for e in $HGTREALPATH/*.hgt ; do
-	srtm2sdf -d /dev/null $e
-	echo "compressing.."
-	nice bzip2 -f *.sdf
-	echo "deleting hgt file.."
-	rm $e
-done
-
-echo "Complete.  The files in $HGTFILEDIR may be removed."
+./get-datafiles.sh "North_America"
 
---
+++ get-datafiles.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+# get the SRTM data files and convert them for splat use
+#
+# Takes one parameter: the continent to retrieve.  Valid values:
+#
+# Africa
+# Australia
+# Eurasia
+# Islands
+# North_America
+# South_America
+
+# path to topgraphy datafiles
+TOPOFILEDIR=splat-datafiles/sdf/
+# local hgt file archive
+HGTFILEDIR=splat-datafiles/hgtzip/
+
+CONTINENT=$1
+case $CONTINENT in
+	North_America|South_America|Africa|Eurasia|Australia|Islands)
+		echo $CONTINENT
+		;;
+	*)
+		echo "Invalid continent: $CONTINENT"
+		exit 1
+		;;
+esac
+
+
+INDEXURL="http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/${CONTINENT}/"
+
+INDEXFILE=`mktemp`
+
+echo "getting index.."
+#wget -q -O - $INDEXURL | \
+#	sed -e '/hgt.zip/!d; s/.* \([NSWE0-9]\+\.\?hgt\.zip\).*$/\1/;' \
+#	> $INDEXFILE
+
+mkdir -p $HGTFILEDIR
+mkdir -p $TOPOFILEDIR
+
+echo "retrieving files.."
+cd $HGTFILEDIR
+#wget -nv -N -B $INDEXURL -i $INDEXFILE
+cd -
+
+rm $INDEXFILE
+
+# to minimize disk space required, run srtm2sdf on each file as it is unzipped.
+
+HGTREALPATH=`realpath $HGTFILEDIR`
+TOPOREALPATH=`realpath $TOPOFILEDIR`
+PWD=`pwd`
+
+echo "unpacking hgt files.."
+cd $HGTFILEDIR
+for e in *.zip ; do 
+	echo $e
+	nice unzip -o $e
+	HGTFILE=`echo $e | sed -e 's/\.\?hgt.zip/.hgt/'`
+	if [ -r $HGTFILE ]; then
+		cd $TOPOREALPATH
+		nice srtm2sdf -d /dev/null $HGTREALPATH/$HGTFILE
+		echo "compressing.."
+		nice bzip2 -f *.sdf
+		echo "deleting hgt file.."
+		cd $HGTREALPATH
+		rm $HGTFILE
+	fi
+done
+
+cd $PWD
+
+echo "Complete.  The files in $HGTFILEDIR may be removed."
+
+
