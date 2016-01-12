75d3963f92e18cdd211507ade8a2f5a595732bf1
molo1134 <molo1134@gmx.com>
2016-01-12 16:47:30 -0500

    add script to retrieve datafiles for the whole world.

---
+++ get-datafiles-world.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+./get-datafiles-northamerica.sh
+./get-datafiles.sh "South_America"
+./get-datafiles.sh "Africa"
+./get-datafiles.sh "Eurasia"
+./get-datafiles.sh "Australia"
+./get-datafiles.sh "Islands"
+
+
